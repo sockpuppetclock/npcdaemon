673a6074a4d0d249c412e85b9d58acf6b648f7cd
sockpuppetclock <42406458+sockpuppetclock@users.noreply.github.com>
2024-03-17 03:04:47 -0500

    gitignorer

--- gmad.bat
+++
@@ -1,2 +0,0 @@
-CD %PROGRAMFILES(X86)%\Steam\steamapps\common\GarrysMod || EXIT
-bin\gmad.exe create -warninvalid -folder garrysmod\addons\npcdaemon -out npcdaemon.gma
--- gmpublish.bat
+++
@@ -1,4 +0,0 @@
-CD %PROGRAMFILES(X86)%\Steam\steamapps\common\GarrysMod || EXIT
-set /p "confirm=UPDATE NPCDAEMON? (y/n) "
-if not %confirm%=="y" exit
-bin\gmpublish.exe update -id 2574407396 -addon npcdaemon.gma
